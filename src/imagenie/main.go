c932de9ca2424967dbd0604403f50ec14ce979ba
Nishant Shelar <nishant8887@gmail.com>
2017-03-25 10:09:04 +0530

    Added image file upload code.

--- src/imagenie/main.go
+++ src/imagenie/main.go
@@ -6,10 +6,14 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/satori/go.uuid"
 	"gopkg.in/yaml.v2"
 	"imagenie/utils"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 type ServiceSettings struct {
@@ -54,7 +58,9 @@ func (self *ImagenieListener) Start() error {
 	defer self.db.Close()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/user/", self.Home)
+	r.HandleFunc("/home", self.Home)
+	r.HandleFunc("/upload", self.UploadFile).Methods("POST")
+
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", utils.NoDirFileServer("static")))
 
 	http.Handle("/", r)
@@ -72,4 +78,41 @@ func main() {
 
 func (self *ImagenieListener) Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world!")
+}
+
+func (self *ImagenieListener) UploadFile(w http.ResponseWriter, r *http.Request) {
+
+	file, header, err := r.FormFile("image_file")
+
+	if err != nil {
+		http.Error(w, "Error in uploading file", http.StatusBadRequest)
+		return
+	}
+
+	defer file.Close()
+
+	extension := filepath.Ext(header.Filename)
+	if extension == "" {
+		http.Error(w, "Only image files can be uploaded", http.StatusBadRequest)
+		return
+	}
+
+	file_id := uuid.NewV4()
+	new_file_name := fmt.Sprintf("/tmp/%s-original%s", file_id, extension)
+
+	out, err := os.Create(new_file_name)
+	if err != nil {
+		http.Error(w, "Unable to create the file for writing.", http.StatusInternalServerError)
+		return
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		http.Error(w, "Error in saving file.", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(200)
 }
