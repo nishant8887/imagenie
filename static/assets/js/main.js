d6a19ed922b87b22d301eefb2397887d513d5509
Nishant Shelar <nishant8887@gmail.com>
2017-03-29 16:27:07 +0530

    Detect upload complete. Replaced tabs with spaces.

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,40 +1,41 @@
 $(document).ready(function() {
-	var MainView = {
-		loggedIn: ko.observable(false),
-		signInBox: ko.observable(true),
-		uploadBox: ko.observable(false),
+    var MainView = {
+        loggedIn: ko.observable(false),
+        signInBox: ko.observable(true),
+        uploadBox: ko.observable(false),
+        uploading: ko.observable(false),
 
-		cuUsername: ko.observable(''),
-		cuFirstname: ko.observable(''),
-		cuLastname: ko.observable(''),
-		cuEmail: ko.observable(''),
-		cuPassword: ko.observable(''),
-		cuConfirmPassword: ko.observable(''),
-		cuError: ko.observable(''),
+        cuUsername: ko.observable(''),
+        cuFirstname: ko.observable(''),
+        cuLastname: ko.observable(''),
+        cuEmail: ko.observable(''),
+        cuPassword: ko.observable(''),
+        cuConfirmPassword: ko.observable(''),
+        cuError: ko.observable(''),
 
-		luUsername: ko.observable(''),
-		luPassword: ko.observable(''),
-		luError: ko.observable(''),
+        luUsername: ko.observable(''),
+        luPassword: ko.observable(''),
+        luError: ko.observable(''),
 
-		uffile: ko.observable(''),
-		ufdescription: ko.observable(''),
+        uffile: ko.observable(''),
+        ufdescription: ko.observable(''),
 
-		images: ko.observableArray([]),
-		page: ko.observable(1),
-		total_pages: ko.observable(1),
+        images: ko.observableArray([]),
+        page: ko.observable(1),
+        total_pages: ko.observable(1),
 
-		showSignIn: function () {
-			this.signInBox(true);
-			this.luError('');
-		},
+        showSignIn: function () {
+            this.signInBox(true);
+            this.luError('');
+        },
 
-		showSignUp: function() {
-			this.signInBox(false);
-			this.cuError('');
-		},
+        showSignUp: function() {
+            this.signInBox(false);
+            this.cuError('');
+        },
 
-		doLogIn: function() {
-			var username = this.luUsername();
+        doLogIn: function() {
+            var username = this.luUsername();
             var password = this.luPassword();
             
             if (username == "" || password == "") {
@@ -64,10 +65,10 @@ $(document).ready(function() {
                     _this.luError("Invalid username or password");
                 }
             });
-		},
+        },
 
-		doCreateAccount: function() {
-			var password = this.cuPassword();
+        doCreateAccount: function() {
+            var password = this.cuPassword();
             var confirm_password = this.cuConfirmPassword();
             var username = this.cuUsername();
             var firstname = this.cuFirstname();
@@ -114,10 +115,10 @@ $(document).ready(function() {
                     _this.cuError("Username or email already exists");
                 }
             });
-		},
+        },
 
-		logOut: function() {
-			var _this = this;
+        logOut: function() {
+            var _this = this;
             $.ajax({
                 method: "POST",
                 url: "/user/logout",
@@ -128,49 +129,61 @@ $(document).ready(function() {
                 error: function(e) {
                 }
             });
-		},
+        },
 
-		toggleUpload: function() {
-			if (!this.loggedIn()) {
-				this.uploadBox(false);
-				return;
-			}
-			if (this.uploadBox()) {
-				this.uploadBox(false);
-			} else {
-				this.uffile('');
-				this.ufdescription('');
-				this.uploadBox(true);
-			}
-		},
+        toggleUpload: function() {
+            if (!this.loggedIn()) {
+                this.uploadBox(false);
+                return;
+            }
+            if (this.uploadBox()) {
+                this.uploadBox(false);
+            } else {
+                this.uffile('');
+                this.ufdescription('');
+                this.uploadBox(true);
+            }
+        },
 
-		doneUpload: function() {
-			this.uploadBox(false);
-			return true;
-		},
+        doneUpload: function() {
+            var _this = this;
+            this.uploading(true);
+            $("#upload_target").load(function () {
+                iframeContents = this.contentWindow.document.body.innerHTML;
+                console.log(iframeContents);
+                if (iframeContents.indexOf("Error") !== -1 || iframeContents.indexOf("error") !== -1) {
+                    // Success in upload
+                } else {
+                    // Error in upload
+                }
+                _this.uploading(false);
+                _this.uploadBox(false);
+            });
+            return true;
+        },
 
-		nextPage: function() {
-			if (this.page() >= this.total_pages()) return;
-			this.page(this.page()+1);
-			this.fetchPage();
-		},
+        nextPage: function() {
+            if (this.page() >= this.total_pages()) return;
+            this.page(this.page()+1);
+            this.fetchPage();
+        },
 
-		previousPage: function() {
-			if (this.page() <= 1) return;
-			this.page(this.page()-1);
-			this.fetchPage();
-		},
+        previousPage: function() {
+            if (this.page() <= 1) return;
+            this.page(this.page()-1);
+            this.fetchPage();
+        },
 
-		fetchPage: function() {
-			var page_no = this.page();
-			console.log("Fetching page: " + page_no);
-			var _this = this;
+        fetchPage: function() {
+            var page_no = this.page();
+            console.log("Fetching page: " + page_no);
+            var _this = this;
             $.ajax({
                 method: "GET",
                 url: "/images/" + page_no,
                 success: function(str) {
-					resp = JSON.parse(str);
-					_this.page(resp.page);
+                    resp = JSON.parse(str);
+                    _this.page(resp.page);
 
                     console.log(resp);
 
@@ -178,23 +191,23 @@ $(document).ready(function() {
                         _this.total_pages(resp.total_pages);
                     }
 
-					_this.images.removeAll();
-					for(var i in resp.images) {
-						var image = resp.images[i];
-						_this.images.push(image);
-					}
+                    _this.images.removeAll();
+                    for(var i in resp.images) {
+                        var image = resp.images[i];
+                        _this.images.push(image);
+                    }
                 },
                 error: function(e) {
                 }
             });
-		}
-	};
+        }
+    };
 
-	ko.applyBindings(MainView);
+    ko.applyBindings(MainView);
 
-	var username = $.cookie("user");
-	if (username != null && username != "") {
-		MainView.loggedIn(true);
-	}
-	MainView.fetchPage();
+    var username = $.cookie("user");
+    if (username != null && username != "") {
+        MainView.loggedIn(true);
+    }
+    MainView.fetchPage();
 });
