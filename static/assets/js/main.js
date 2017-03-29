422f1d4c1f3d5f5e61de1cb3dfe1dd085b4b1013
Nishant Shelar <nishant8887@gmail.com>
2017-03-29 23:26:49 +0530

    Always reload ui on upload action

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -153,11 +153,11 @@ $(document).ready(function() {
                 if (iframeContents.indexOf("Error") !== -1 || iframeContents.indexOf("error") !== -1) {
                     // Success in upload
                     console.log("Upload complete");
-                    setTimeout(fetchPage, 2000);
                 } else {
                     // Error in upload
                     console.log("Upload error");
                 }
+                setTimeout(_this.fetchPage, 2000);
                 _this.uploading(false);
                 _this.uploadBox(false);
             });
